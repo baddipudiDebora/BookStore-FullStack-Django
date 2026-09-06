6fb6d8e97ba8a4451377fac67db757330b66a69b
CA-PW0FY6QZ\Debora Baddipudi <deboraswaroop.baddipudi@gmail.com>
2026-09-06 17:50:42 -0400

    chore: updated settings.py Django configuration error: django-allauth

--- online_book/settings.py
+++ online_book/settings.py
@@ -54,15 +54,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',  # Add this line here
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-          'whitenoise.middleware.WhiteNoiseMiddleware',
- 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'online_book.urls'
