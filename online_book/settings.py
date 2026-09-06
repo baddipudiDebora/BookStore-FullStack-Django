b17ccf7bc555d09d888d78d75be56c3881418831
CA-PW0FY6QZ\Debora Baddipudi <deboraswaroop.baddipudi@gmail.com>
2026-09-06 17:57:19 -0400

    updated allowed host to local host from previous heroku app

--- online_book/settings.py
+++ online_book/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-temporary-key-for-int
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'TRUE' 
 
-ALLOWED_HOSTS = ['djangopractisedemo.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
